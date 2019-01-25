c4d70d3405219812acacab6d62d4b3d2cb1c4a88
alexa1999 <anel.veliz@ucsp.edu.pe>
2019-01-25 08:48:45 -0500

    Add files via upload

--- 9/main.cpp
+++ 9/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 using namespace std;
-//Sobrecargar el operador << para que funcione la funciÃ³n print de la clase DynamicArray
+//Sobrecargar el operador << para que funcione la función print de la clase DynamicArray
 //Implementar la Clase USCurrency (vista en clase), sobrecargar los operadores: +, -, +=, -=. <<
 //Crear un objeto DynamicArray de USCurrency's. Deberia funcionar todo sin problemas.
 class USCurrency {
@@ -30,36 +30,36 @@ public:
         }
         return tmp;
     }
-    USCurrency& operator -(const USCurrency &m, const USCurrency &o) {
-        USCurrency tmp ;
-        tmp.cents = m.cents - o.cents;
-        tmp.dollars = m.dollars - o.dollars;
-
-        if(tmp.cents >= 100) {
-            tmp.dollars = 1;
-            tmp.cents += 100;
-        }
-        return tmp;
-    }
-    USCurrency& operator +=(const USCurrency &m, const USCurrency &o) {
-        USCurrency tmp ;
-        tmp.cents = m.cents + o.cents;
-        tmp.dollars = m.dollars + o.dollars;
-
-        if(tmp.cents >= 100) {
-            tmp.dollars += 1;
-            tmp.cents -= 100;
-        }
-        return tmp;
-    }
+//    USCurrency& operator -(const USCurrency &m, const USCurrency &o) {
+//        USCurrency tmp ;
+//        tmp.cents = m.cents - o.cents;
+//        tmp.dollars = m.dollars - o.dollars;
+//
+//        if(tmp.cents <= 100) {
+//            tmp.dollars = 1;
+//            tmp.cents += 100;
+//        }
+//        return tmp;
+//    }
+//    USCurrency& operator +=(const USCurrency &m, const USCurrency &o) {
+//        USCurrency tmp ;
+//        tmp.cents = m.cents + o.cents;
+//        tmp.dollars = m.dollars + o.dollars;
+//
+//        if(tmp.cents >= 100) {
+//            tmp.dollars += 1;
+//            tmp.cents -= 100;
+//        }
+//        return tmp;
+//    }
 ostream& operator<<(ostream &output, const USCurrency &o)
 {
     output << "$" << o.dollars << "." << o.cents;
     return output;
 }
 int main() {
-    USCurrency a = {2, 50};
-    USCurrency b = {1, 75};
+    USCurrency a(2, 50);
+    USCurrency b(1, 75);
     USCurrency c = a + b;
     cout << c << endl;
     return 0;
