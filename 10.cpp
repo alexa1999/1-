bdf6d90c4b8d6c47f9ae4f6b1fe573dfe6a29405
alexa1999 <anel.veliz@ucsp.edu.pe>
2019-01-29 10:31:34 -0500

    Add files via upload

--- 10.cpp
+++ 10.cpp
@@ -60,50 +60,171 @@ using namespace std;
 //	return 0;
 //}
 
+//class Polygon
+//{
+//protected:
+//	int width,height;
+//public:
+//	void set_values(int a,int b){width=a ;height =b ;}
+//	virtual int area(){return 0;}
+//		
+//};
+//class Rectangle:public Polygon{
+//public:
+//	int area(){return width*height;}
+//};
+//class Triangle:public Polygon{
+//public:
+//	int area(){return width*height /2;}
+//};
+//int main(int argc, char *argv[]) {
+//	Rectangle rect;
+//	Triangle trlg;
+//	//antes no habia area
+////////	rect.set_values(4,5);
+////////	trlg.set_values(4,5);
+////////	cout<<rect.area()<<endl;
+////////	cout<<trlg.area()<<endl;
+////////	//dieferente dforma
+////////	Rectangle *ptr=&rect;
+////////	ptr->set_values(4,5);
+////////	cout<<ptr->area()<<endl;
+////////	//diferente forma
+////////	Triangle &ref=trlg;
+////////	ref.set_values(4,5);
+////////	cout<<ref.area()<<endl;
+//	//con area 
+//	Polygon pol;
+//////	Polygon * p1=&rect;
+//////	Polygon * p2=&trlg;
+//////	Polygon * p3=&pol;
+//////	p1->set_values(4,5);
+//////	p2->set_values(4,5);
+//////	p3->set_values(4,5);
+//	
+//	Polygon &p1=rect;
+//	Polygon &p2=trlg;
+//	Polygon &p3=pol;
+//	p1.set_values(4,5);
+//	p2.set_values(4,5);
+//	p3.set_values(4,5);
+//	
+//	cout<<p1.area()<<endl;
+//	
+//	cout<<p2.area()<<endl;
+//	cout<<p3.area()<<endl;
+//	
+//	
+//	return 0;
+//}
+//
+//class Polygon
+//{
+//protected:
+//	int width,height;
+//public:
+//	void set_values(int a,int b){width=a ;height =b ;}
+//	virtual int area(void)=0;
+//	
+//};
+//class Rectangle:public Polygon{
+//public:
+//	int area(){return width*height;}
+//};
+//class Triangle:public Polygon{
+//public:
+//	int area(){return width*height /2;}
+//};
+//int main(int argc, char *argv[]) {
+//	Rectangle rect;
+//	Triangle trlg;
+//
+//	
+//	////	Polygon * p1=&rect;
+//	////	Polygon * p2=&trlg;
+//	////	Polygon * p3=&pol;
+//	////	p1->set_values(4,5);
+//	////	p2->set_values(4,5);
+//	////	p3->set_values(4,5);
+//	
+//	Polygon *p1=&rect;
+//	Polygon *p2=&trlg;
+//	
+//	p1->set_values(4,5);
+//	p2->set_values(4,5);
+//
+//	
+//	cout<<p1->area()<<endl;
+//	
+//	cout<<p2->area()<<endl;
+//	
+//	
+//	
+//	return 0;
+//}
+
+//class Polygon
+//{
+//protected:
+//	int width,height;
+//public:
+//	void set_values(int a,int b){width=a ;height =b ;}
+//	virtual int area(void)=0;
+//	void printarea(){ cout<<this->area()<<endl;}
+//};
+//class Rectangle:public Polygon{
+//public:
+//	int area(void){return width*height;}
+//};
+//class Triangle:public Polygon{
+//public:
+//	int area(void){return width*height /2;}
+//};
+//int main(int argc, char *argv[]) {
+//	Rectangle rect;
+//	Triangle trlg;
+//	
+//	Polygon *p1=&rect;
+//	Polygon *p2=&trlg;
+//	
+//	p1->set_values(4,5);
+//	p2->set_values(4,5);
+//	p1->printarea();
+//	p2->printarea();
+//	
+//	
+//	
+//	return 0;
+//}
 class Polygon
 {
 protected:
 	int width,height;
 public:
-	void set_values(int a,int b){width=a ;height =b ;}
-	virtual int area(){return 0;}
-		
+	Polygon(int x,int y):width(x),height(y){}
+
+	virtual int area(void)=0;
+	void printarea(){ cout<<this->area()<<endl;}
 };
 class Rectangle:public Polygon{
 public:
-	int area(){return width*height;}
+	Rectangle(int a,int b):Polygon(a,b){}
+	int area(void){return width*height;}
 };
 class Triangle:public Polygon{
 public:
-	int area(){return width*height /2;}
+	int area(void){return width*height /2;}
 };
 int main(int argc, char *argv[]) {
-	Rectangle rect;
-	Triangle trlg;
-//	rect.set_values(4,5);
-//	trlg.set_values(4,5);
-//	cout<<rect.area()<<endl;
-//	cout<<trlg.area()<<endl;
-//	//dieferente dforma
-//	Rectangle *ptr=&rect;
-//	ptr->set_values(4,5);
-//	cout<<ptr->area()<<endl;
-//	//diferente forma
-//	Triangle &ref=trlg;
-//	ref.set_values(4,5);
-//	cout<<ref.area()<<endl;
-	Polygon pol;
-	Polygon * p1=&rect;
-	Polygon * p2=&trlg;
-	Polygon * p3=&pol;
-	p1->set_values(4,5);
-	p2->set_values(4,5);
-	p3->set_values(4,5);
-	cout<<rect.area()<<endl;
-	cout<<p1->area()<<endl;
-	cout<<p2->area()<<endl;
-	cout<<p3->area()<<endl;
 	
+	
+	Polygon *p1=new Rectangle(4,5);
+	Polygon *p2=new Triangle(4,5);
+	
+	p1->printarea();
+	p2->printarea();
+	
+	//delete[]
 	
 	return 0;
 }
